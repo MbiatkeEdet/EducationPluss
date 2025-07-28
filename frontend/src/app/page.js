47039c8984ce5148244aef0135a367f32be1b5ad
Bob-Manuel McLeod Sotonye <62424635+sotonye-m@users.noreply.github.com>
2025-07-28 13:34:15 +0100

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,23 +2,19 @@ import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Product from "@/components/Product";
 import Services from "@/components/Services";
-import Head from 'next/head';
+
+export const metadata = {
+  title: "Finear | AI-Powered Learning with Crypto Rewards",
+  description: "Revolutionize your learning with AI-powered tools and earn crypto rewards for achieving your academic goals.",
+};
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Finear | AI-Powered Learning with Crypto Rewards</title>
-        <meta name="description" content="Revolutionize your learning with AI-powered tools and earn crypto rewards for achieving your academic goals." />
-      </Head>
-      
-      <main>
-        <Hero />
-        <Product />
-        <Services />
-      </main>
-      
+    <main>
+      <Hero />
+      <Product />
+      <Services />
       <Footer />
-    </>
-  )
+    </main>
+  );
 }
